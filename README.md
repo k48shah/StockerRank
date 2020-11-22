e5b0845963df77a22e2e14677c351675912238a2
k48shah <48638232+k48shah@users.noreply.github.com>
2020-11-22 18:04:59 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+A Stock screener/ranker that is developed by small group of Waterloo students by using the fundamental principles borrowed from "The Little Book that Still Beats the Market" by Joel Greenblatt
+
+How it Works:
+1. Import required python libraries
+2. Add a list of stocks in Column A of StockScreeneer.xlsx
+3. Run StockScreener.py
+4. Once data is collected the data will be exported to a file called 'stonks.xls'
+5. Sort data in column B by lowest to greatest to see the top pick (best ranked) stocks
+
+The current 30 top pick stocks (as of 21/11/2020) have beaten the S&P500 by a margin of 4x in percent change for the past year.
+
+The graph below represents the percent change in the top 30 stocks vs the bottom 30 stocks chosen by the program. It also uses the S&P500 as a control.
+![alt text](https://imgur.com/a/tPt0PJD)
+
+
+TO-DO LIST:
+- Populate Yahoo finance portfolios using selenium and set # of cash
+- Take top 30 stocks taken from algorithm, find their percentage gain averages over different time frames and compare to s&p 500 during same frames
+- Introduce technical analysis by using OpenCL
+- Asynchronous searching of stocks to reduce wait time for data
+- Create GUI to choose action from the stock screener
+- Append % changes to the end of stonks.xls
+- Rename stonks.xls according to initial file given and date
+- Comment code functions for readability
+- make a main python file which everything is called from rather than a single file which houses all code
+- Integrate a front end web server to display and work through the stock picks
+- Auto-sort data before dumping into stonks.xls
+- Add ability for user to choose their second comparison tool
+- Remove stocks that do not have one or two of the required entries
+
+This is an educational project and none of the developers are responsible for any trades or decisions made from the tool
---
+++ StockScreener.py
@@ -0,0 +1,134 @@
+import xlrd
+import xlwt
+from yahooquery import Ticker
+import os
+
+def getStocksFromCSV():
+    raw_ticker = xlrd.open_workbook("StockScreener.xlsx")
+    sheet = raw_ticker.sheet_by_index(0)
+    dataFrame = list()
+    for x in range(sheet.nrows):
+        dataFrame.append(sheet.cell_value(x, 0))
+    return dataFrame
+
+def exportToCSV(valueList, fileName):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet("STONKS")
+    for i, x in enumerate(valueList):
+        for j, y in enumerate(x):
+            sheet.write(i, j, x[j])
+    book.save(fileName + ".xls")
+
+
+def tickerEarningsYield(stockList):
+    earningsYield = list()
+    for stock in stockList:
+        print(stock)
+        stockInfo = Ticker(stock)
+        try:
+            if(int(str(stockInfo.price).split('\'regularMarketTime\': \'')[1].split('-')[0]) >= 2020):
+                #latestEarnings = str(stockInfo.balance_sheet(frequency="q").get("RetainedEarnings")).split(" ")[-4].split("N")[0]
+                #sharePrice = str(stockInfo.price).split('\'regularMarketPrice\': ')[1].split(',')[0]
+                #eps = float(latestEarnings)/float(sharePrice)
+                print(1/float(str(stockInfo.summary_detail).split('\'forwardPE\': ')[1].split(',')[0]) * 100)
+                earningsYield.append(1/float(str(stockInfo.summary_detail).split('\'forwardPE\': ')[1].split(',')[0]) * 100)
+        #     print(EarningsYield[index])
+            else:
+                earningsYield.append(-100000000)
+        except:
+            earningsYield.append(-100000000)
+    return earningsYield
+
+# def tickerComp(stockList):
+#     returnOnAssets = list()
+#     for stock in stockList:
+#         print(stock)
+#         stockInfo = Ticker(stock)
+#         try:
+#             returnOnAssets.append(float(str(stockInfo.financial_data).split('\'returnOnAssets\': ')[1].split(',')[0]))
+#             print(float(str(stockInfo.financial_data).split('\'returnOnAssets\': ')[1].split(',')[0]))
+#         except:
+#             returnOnAssets.append(-100000)
+#             print("stonk not real2: " + str(stock))
+#     return returnOnAssets
+
+def tickerComp(stockList, financialStr):
+    ComparisonVal = list()
+    for stock in stockList:
+        print(stock)
+        stockInfo = Ticker(stock)
+        try:
+            if(int(str(stockInfo.price).split('\'regularMarketTime\': \'')[1].split('-')[0]) >= 2020):
+                ComparisonVal.append(float(str(stockInfo.financial_data).split('\'' + financialStr + '\': ')[1].split(',')[0]))
+                print(float(str(stockInfo.financial_data).split('\'' + financialStr + '\': ')[1].split(',')[0]))
+            else:
+                ComparisonVal.append(-10000000)
+        except:
+            ComparisonVal.append(-10000000)
+    return ComparisonVal
+
+# def tickerROE(stockList):
+#     returnOnEquity = list()
+#     for stock in stockList:
+#         print(stock)
+#         stockInfo = Ticker(stock)
+#         try:
+#             returnOnEquity.append(float(str(stockInfo.financial_data).split('\'returnOnEquity\': ')[1].split(',')[0]))
+#             print(float(str(stockInfo.financial_data).split('\'returnOnEquity\': ')[1].split(',')[0]))
+#         except:
+#             returnOnEquity.append(-100000)
+#             print("stonk not real2: " + str(stock))
+#     return returnOnEquity
+
+def rankVal(rankList):
+    sortedList = sorted(rankList, reverse=True)
+    ranks = list()
+    for i, x in enumerate(rankList):
+        ranks.append(sortedList.index(x))
+    return ranks
+
+def sumRanks(list1, list2):
+    sumList = list()
+    for i in range(len(list1)):
+        sumList.append(list1[i] + list2[i])
+    return sumList
+
+def findBest(rankList, stockList):
+    storeStock = stockList[rankList.index(min(rankList))]
+    storeRank = min(rankList)
+    #stockList.remove(stockList[rankList.index(min(rankList))])
+    #rankList.remove(min(rankList))
+    rankList[rankList.index(min(rankList))] = 10000000
+    return [storeStock, storeRank]
+
+#TODO
+def removeDepStocks():
+    return
+
+
+stockListing = getStocksFromCSV()
+constStockListing = stockListing
+eYield = tickerEarningsYield(stockListing)
+earningsRank = rankVal(eYield)
+
+# roa = tickerROA(stockListing)
+# roaRank = rankVal(roa)
+
+# roe = tickerROE(stockListing)
+# roeRank = rankVal(roe)
+
+secondComp = tickerComp(stockListing, 'returnOnAssets')
+secondRank = rankVal(secondComp)
+
+cumRanks = sumRanks(earningsRank, secondRank)
+print(cumRanks)
+pickList = list()
+for x in range(len(stockListing)):
+    bestVal = findBest(cumRanks, stockListing)
+    originalIndex = stockListing.index(bestVal[0])
+    pickList.append([bestVal[0], bestVal[1], eYield[originalIndex], earningsRank[originalIndex], secondComp[originalIndex], secondRank[originalIndex]])
+    print(pickList[x])
+print(len(eYield), len(earningsRank), len(secondRank))
+exportToCSV(pickList, "stonks")
+# Only do once to prevent extreme stock removals
+# removeDepStocks(stockListing)
