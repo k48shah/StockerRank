734116464df3d82136588c70788acb02bc809a31
k48shah <48638232+k48shah@users.noreply.github.com>
2021-01-18 02:54:28 -0500

    Added Priority, stock interpretter, testing for best returns depending on priorities. Added extra filters for screener. Changed file naming scheme

--- StockScreener.py
+++ StockScreener.py
@@ -1,5 +1,4 @@
 import xlrd
-import xlwt
 from yahooquery import Ticker
 import json
 
@@ -34,9 +33,10 @@ def tickerInfo(stockList, finList):
                 #eps = float(latestEarnings)/float(sharePrice)
                 finData = stockInfo.financial_data[stock]
                 keyStats = stockInfo.key_stats[stock]
+                valMeasures = stockInfo.valuation_measures
                 for index, string in enumerate(finList):
                     infoBool = 0
-                    if (string == "forwardEP" and ("forwardPE" in keyStats)):
+                    if (string == "forwardPE" and ("forwardPE" in keyStats)):
                         assetList[index].append(1/keyStats["forwardPE"] * 100)
                         infoBool += 1
                     elif (string == "returnOnAssets" and ("returnOnAssets" in finData)):
@@ -48,6 +48,23 @@ def tickerInfo(stockList, finList):
                     elif (string == "earningsYield" and ("enterpriseValue" in keyStats) and ("ebitda" in finData)):
                         assetList[index].append(finData["ebitda"]/keyStats["enterpriseValue"])
                         infoBool += 1
+                    elif (string == "prevEarningsYield" and ("PeRatio" in valMeasures)):
+                        try:
+                            if(str(1/valMeasures["PeRatio"][0] * 100) != "nan"):
+                                assetList[index].append(1/valMeasures["PeRatio"][0] * 100)
+                            elif(str(1/valMeasures["PeRatio"][1] * 100) != "nan"):
+                                assetList[index].append(1/valMeasures["PeRatio"][0] * 100)
+                            else:
+                                assetList[index].append(-100000000)
+                        except:
+                            assetList[index].append(-100000000)
+                        infoBool += 1
+                    elif (string == "CPS" and ("totalCashPerShare" in finData)):
+                        assetList[index].append(finData["totalCashPerShare"])
+                    elif (string == "bookToPrice" and ("priceToBook" in keyStats)):
+                        assetList[index].append(1/keyStats["priceToBook"])
+                    elif (string == "pegRatio" and ("pegRatio" in keyStats)):
+                        assetList[index].append(1/keyStats["pegRatio"])
                     elif (infoBool == 0):
                         assetList[index].append(-100000000)
             else:
@@ -62,13 +79,14 @@ def tickerInfo(stockList, finList):
     return assetList
 
 
-def rankVal(rankList):
+def rankVal(rankList, mult):
+    mult = [0.1, 0.9]
     ranks = [[] for _ in rankList]
     for index, val in enumerate(rankList):
         sortedList = sorted(val, reverse=True)
         print(val)
         for i, x in enumerate(val):
-            ranks[index].append(sortedList.index(x))
+            ranks[index].append(int(sortedList.index(x)/mult[index]))
     return ranks
 
 def sumRanks(rankedList):
@@ -90,27 +108,52 @@ def findBest(rankList, stockList):
 def removeDepStocks():
     return
 
+def fileNameChange(filter, priority):
+    fileName = ""
+    for i in range(len(filter)):
+        filePriority = str(priority[i])
+        if filePriority.find('.'):
+            filePriority.replace('.', '_')
+        fileName = fileName + filter[i] + filePriority
+    return fileName
 
-stockListing = getStocksFromCSV()
-strList = ["earningsYield", "returnOnAssets"]
-filterList = tickerInfo(stockListing, strList)
-rankList = rankVal(filterList)
+def main(stockListing, strList, multiplier):
+    if not (len(strList) == len(multiplier)):
+        print("Check argument lengths!")
+    else:
+        for i, x in enumerate(multiplier):
+            if x == 0:
+                strList.pop(i)
+                multiplier.pop(i)
+        for i, x in enumerate(multiplier):
+            if x == 0:
+                strList.pop(i)
+                multiplier.pop(i)
+        filterList = tickerInfo(stockListing, strList)
+        rankList = rankVal(filterList, multiplier)
 
-cumRanks = sumRanks(rankList)
-print(cumRanks)
-pickList = list()
-for x in range(len(stockListing)):
-    bestVal = findBest(cumRanks, stockListing)
-    originalIndex = stockListing.index(bestVal[0])
-    temp = list()
-    temp.append(bestVal[0])
-    temp.append(bestVal[1])
-    for index, filter in enumerate(strList):
-        temp.append(filterList[index][originalIndex])
-        temp.append(rankList[index][originalIndex])
-    pickList.append(temp)
-    print(pickList[x])
-sortedList = sorted(pickList, key=lambda x: x[1])
-exportToJSON(sortedList, "stonks", strList)
-# Only do once to prevent extreme stock removals
-# removeDepStocks(stockListing)
+        cumRanks = sumRanks(rankList)
+        print(cumRanks)
+        pickList = list()
+        for x in range(len(stockListing)):
+            bestVal = findBest(cumRanks, stockListing)
+            originalIndex = stockListing.index(bestVal[0])
+            temp = list()
+            temp.append(bestVal[0])
+            temp.append(bestVal[1])
+            for index, filter in enumerate(strList):
+                temp.append(filterList[index][originalIndex])
+                temp.append(rankList[index][originalIndex])
+            pickList.append(temp)
+            print(pickList[x])
+        sortedList = sorted(pickList, key=lambda x: x[1])
+        fileString = fileNameChange(strList, multiplier)
+        exportToJSON(sortedList, fileString, strList)
+
+
+stockList = getStocksFromCSV()
+valueList = ["forwardPE", "returnOnAssets"]
+#Try to keep max priorities to 1, set all to 1 to have equal priorities!
+# 0 is lowest priority, 1 is highest priority (exceeding 1 is equivalent to reducing priority on the opposing filter
+priority = [1, 1]
+main(stockList, valueList, priority)
